2c93f5e4407c6b358635c0f5452b28d65782fd34
Jamie Wright <jamie@brilliantfantastic.com>
2019-01-26 15:15:15 -0500

    Raise an error if the number of clues do not match the number of questions

--- lib/escape/cli.rb
+++ lib/escape/cli.rb
@@ -6,6 +6,8 @@ module Escape
     def start
       config = Escape::Configuration.load("./configuration.yml")
 
+      raise RuntimeError, "Number of questions does not match the number of clues" if config.questions.count != config.clues.count
+
       Escape::Commands::Welcome.new(shell).show(config.welcome_message)
       Escape::Commands::Questions.new(shell).start(config.questions, config.clues)
     end
