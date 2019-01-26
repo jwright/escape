299fe089b807274570deb7496652895887d1bd42
Jamie Wright <jamie@brilliantfantastic.com>
2019-01-26 14:36:43 -0500

    Display a hard coded correct and incorrect clue for the answer

--- lib/escape/commands/questions.rb
+++ lib/escape/commands/questions.rb
@@ -2,10 +2,21 @@ module Escape
   module Commands
     class Questions < Base
       def start(questions)
-        questions.each do |question|
+        questions.each_with_index do |question, index|
           answer = stream.ask(question.text)
+          stream.say(question.correct?(answer) ? correct_clue(index) : incorrect_clue(index))
           stream.say
         end
+      end
+
+      private
+
+      def correct_clue(index)
+        "Y"
+      end
+
+      def incorrect_clue(index)
+        "N"
       end
     end
   end
--- spec/escape/commands/questions_spec.rb
+++ spec/escape/commands/questions_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Escape::Commands::Questions do
   include Spec::Helpers::OutputHelper
 
   describe "#start" do
-    let(:question1) { double(text: "What is the meaning of life?")}
+    let(:question1) { double(text: "What is the meaning of life?", correct?: true)}
     let(:question_list) { [question1]}
     let(:stream) { Thor::Shell::Basic.new }
     subject { described_class.new(stream) }
@@ -12,7 +12,20 @@ RSpec.describe Escape::Commands::Questions do
     it "displays the first question" do
       expect(Thor::LineEditor).to receive(:readline).with("What is the meaning of life? ", {}).and_return("IDK")
 
-      subject.start(question_list)
+      capture(:stdout) { subject.start(question_list) }
+    end
+
+    it "displays the correct clue if the answer is correct" do
+      allow(Thor::LineEditor).to receive(:readline).with("What is the meaning of life? ", {}).and_return("42")
+
+      expect(capture(:stdout) { subject.start(question_list) }).to match(/Y/)
+    end
+
+    it "displays an incorrect clue if the answer is incorrect" do
+      allow(question1).to receive(:correct?).and_return false
+      allow(Thor::LineEditor).to receive(:readline).with("What is the meaning of life? ", {}).and_return("56")
+
+      expect(capture(:stdout) { subject.start(question_list) }).to_not match(/C/)
     end
   end
 end
