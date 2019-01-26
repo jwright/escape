9b0fd9c8ecd5f4339dffcdbdd0c987421b771745
Jamie Wright <jamie@brilliantfantastic.com>
2019-01-26 14:16:58 -0500

    Load the answer for a question

--- lib/escape/question.rb
+++ lib/escape/question.rb
@@ -2,13 +2,14 @@ module Escape
   class Question
     attr_reader :text
 
-    def initialize(text)
+    def initialize(text, answer: nil)
       @text = text
+      @answer = answer
     end
 
     class << self
       def load(question)
-        new(question["text"])
+        new(question["text"], answer: question["answer"])
       end
     end
   end
