cf7e1d61bbd8241636aa133098a9794501b0918d
Jamie Wright <jamie@brilliantfantastic.com>
2019-01-26 16:18:55 -0500

    Add the ability to quit instead of restart

--- lib/escape/commands/restart.rb
+++ lib/escape/commands/restart.rb
@@ -1,10 +1,12 @@
 module Escape
   module Commands
     class Restart < Base
-      def restart
-        runner.ask "Hit RETURN to restart"
-        runner.say
-        runner.execute :restart
+      def restart_or_quit
+        response = runner.ask "Hit RETURN to restart or Q to quit"
+        unless (response.to_s.strip.upcase == "Q")
+          runner.say
+          runner.execute :restart
+        end
       end
     end
   end
--- spec/escape/commands/restart_spec.rb
+++ spec/escape/commands/restart_spec.rb
@@ -1,18 +1,26 @@
 RSpec.describe Escape::Commands::Restart do
-  describe "#restart" do
+  describe "#restart_or_quit" do
     let(:runner) { double(ask: nil, execute: nil, say: nil) }
     subject { described_class.new(runner) }
 
     it "outputs the option" do
-      expect(runner).to receive(:ask).with("Hit RETURN to restart")
+      expect(runner).to receive(:ask).with("Hit RETURN to restart or Q to quit")
 
-      subject.restart
+      subject.restart_or_quit
     end
 
-    it "invokes a restart" do
+    it "invokes a restart if it was not quit" do
       expect(runner).to receive(:execute).with(:restart)
 
-      subject.restart
+      subject.restart_or_quit
+    end
+
+    it "does not invoke a restart when quit" do
+      allow(runner).to receive(:ask).with("Hit RETURN to restart or Q to quit").and_return("Q")
+
+      expect(runner).to_not receive(:execute)
+
+      subject.restart_or_quit
     end
   end
 end
