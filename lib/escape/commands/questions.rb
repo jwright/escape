b24d2b140aa2b535c10bc5db801496768d00f525
Jamie Wright <jamie@brilliantfantastic.com>
2019-01-26 15:00:38 -0500

    Use clue class

--- lib/escape/commands/questions.rb
+++ lib/escape/commands/questions.rb
@@ -4,19 +4,9 @@ module Escape
       def start(questions, clue)
         questions.each_with_index do |question, index|
           answer = stream.ask(question.text)
-          stream.say(question.correct?(answer) ? correct_clue(index) : incorrect_clue(index))
+          stream.say(question.correct?(answer) ? Clue.new(clue).correct(index) : Clue.new(clue).incorrect(index))
           stream.say
         end
-      end
-
-      private
-
-      def correct_clue(index)
-        "Y"
-      end
-
-      def incorrect_clue(index)
-        "N"
       end
     end
   end
